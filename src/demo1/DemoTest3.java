9212513fc1635c2ae7791f9e324ef565b9acb65f
ericw <ericw@UncleRicco.san.rr.com>
2025-01-29 18:07:49 -0800

    added print function

--- src/demo1/DemoTest3.java
+++ src/demo1/DemoTest3.java
@@ -3,5 +3,8 @@ package demo1;
 public class DemoTest3 {
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		String steam = "We're in this now!";
+		System.out.println(steam);
 }
 }
